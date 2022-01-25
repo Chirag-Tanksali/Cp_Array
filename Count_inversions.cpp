5d1faf1355daa19e4012c0f755dfb6e9502dbdd7
ChiragTanksali <chirag.tanksali@gmail.com>
2022-01-25 23:26:54 +0530

    first commit

---
+++ Count_inversions.cpp
@@ -0,0 +1,72 @@
+/*
+For a given integer array/list 'ARR' of size 'N', find the total number of 'Inversions' that may exist.
+An inversion is defined for a pair of integers in the array/list when the following two conditions are met.
+A pair ('ARR[i]', 'ARR[j]') is said to be an inversion when:
+
+1. 'ARR[i] > 'ARR[j]' 
+2. 'i' < 'j'
+
+Where 'i' and 'j' denote the indices ranging from [0, 'N').
+Input Format :
+The first line of input contains an integer 'N', denoting the size of the array.
+
+The second line of input contains 'N' integers separated by a single space, denoting the elements of the array 'ARR'.
+Output Format :
+Print a single line containing a single integer that denotes the total count of inversions in the input array.
+Note:
+You are not required to print anything, it has been already taken care of. Just implement the given function.  
+Constraints :
+1 <= N <= 10^5 
+1 <= ARR[i] <= 10^9
+
+Where 'ARR[i]' denotes the array element at 'ith' index.
+
+Time Limit: 1 sec
+Sample Input 1 :
+3
+3 2 1
+Sample Output 1 :
+3
+Explanation Of Sample Output 1:
+We have a total of 3 pairs which satisfy the condition of inversion. (3, 2), (2, 1) and (3, 1).
+Sample Input 2 :
+5
+2 5 1 3 4
+Sample Output 2 :
+4
+Explanation Of Sample Output 1:
+We have a total of 4 pairs which satisfy the condition of inversion. (2, 1), (5, 1), (5, 3) and (5, 4).
+*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        
+        int i,count=0;
+        
+        for(i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+            if(a[i]>a[j] && i<j){
+                count++;
+            }
+        }
+        }
+        
+        cout<<count;
+    }
+    return 0;
+}
